27de36102c907a5ae63c2f86350b85b4bd4b6790
SirWojtek <momatoku@gmail.com>
2016-08-23 20:56:02 +0200

    introduced ng-modal

--- frontend/systemjs.config.js
+++ frontend/systemjs.config.js
@@ -8,7 +8,8 @@
     'app':                        'app', // 'dist',
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+    'rxjs':                       'node_modules/rxjs',
+    'ng2-bs3-modal':              'node_modules/ng2-bs3-modal'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
@@ -41,6 +42,7 @@
   // Add package entries for angular packages
   ngPackageNames.forEach(setPackageConfig);
   var config = {
+    defaultJSExtensions: true,
     map: map,
     packages: packages
   };
