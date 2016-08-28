4349f6ce4366bd93abd4ab7db46bb03d3523504b
SirWojtek <momatoku@gmail.com>
2016-08-28 17:28:32 +0200

    removed old code from backend

--- backend/server.js
+++ backend/server.js
@@ -32,47 +32,6 @@ else
     db.close();
 }
 
-// build a simple form
-var pageHTML =
-'<html>' +
-    '<head>' +
-        '<title>D20Site v. 0.1</title>' +
-        '<meta charset="utf-8">' +
-    '</head>' +
-    '<body>' +
-        '<form method="post" action="">' +
-            '<div>' +
-                '<label for="nickname">Nickname:</label>' +
-                '<input type="text" name="nickname">' +
-            '</div>' +
-            '<div>' +
-                '<input type="submit" value="send it">' +
-            '</div>' +
-        '</form>' +
-    '</body>' +
-'</html>';
-
-app.get('/', function (req, res) {
-    // res.sendFile("index.html", {root: path.join("./frontend/pages/index/view/")});
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(pageHTML + randomThing); // serve our HTML code
-});
-
-app.post('/', function (req, res) {
-    var requestData = '';
-    req.setEncoding('utf-8');
-
-    req.on('data', function(data) {
-        requestData += data;
-    });
-
-    req.on('end', function() {
-        var postData = qs.parse(requestData);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Your nick: '+ postData.nickname + '</h1>');
-    });
-});
-
 app.get('/api', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');  // NOTE: this is hack to fit CORS
