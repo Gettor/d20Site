02add8b40713f78f3564d53b1e0927eaa104a20b
SirWojtek <momatoku@gmail.com>
2016-09-24 22:15:23 +0200

    miniatures in find monster

--- frontend/app/monsters/find-monsters.component.ts
+++ frontend/app/monsters/find-monsters.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
 import { CompleterData } from 'ng2-completer';
 import { MonstersService } from './monsters.service';
+import { Miniature } from './../shared/miniature/miniature';
 
 @Component({
    selector : 'find-monsters',
    template : `
       <h1>Find monsters</h1>
-      <ng2-completer [(ngModel)]="searchStr" [dataService]="findService" [minSearchLength]="0"></ng2-completer>
+      <form (ngSubmit)="onSubmit()">
+         <ng2-completer [(ngModel)]="searchStr" [dataService]="findService" [minSearchLength]="0" name="autocomplete"></ng2-completer>
+      </form>
+      <miniature-container [miniatures]="monsterMiniatures"></miniature-container>
    `
 })
 
 export class FindMonstersComponent {
    private searchStr: string;
    private findService : CompleterData;
+   private monsterMiniatures : Miniature[] = [];
 
    constructor(private monstersService: MonstersService) {
       this.findService = monstersService.getFindService();
+   }
+
+   onSubmit() {
+      this.monsterMiniatures.push(new Miniature('aaa', 'empty', 'dasdasd'));
+      return;
    }
 }
 
