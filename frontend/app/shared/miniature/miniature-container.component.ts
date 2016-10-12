fc40f03a3883cb964bc7cfeea5711ccdc53d0b98
SirWojtek <momatoku@gmail.com>
2016-10-12 19:16:47 +0200

    more customisable miniatures

--- frontend/app/shared/miniature/miniature-container.component.ts
+++ frontend/app/shared/miniature/miniature-container.component.ts
@@ -4,20 +4,20 @@ import { Miniature } from './miniature';
 @Component({
    selector : 'miniature-container',
    template : `
-      <div class="container-fluid">
-          <div class="row">
-              <div class="col-md-3" *ngFor="let miniature of miniatures
-               | paginate : { itemsPerPage : 16, currentPage : p}">
-                     <miniature-show [miniature]="miniature"></miniature-show>
-              </div>
-          </div>
-          <div class="row" style="text-align:center">
-              <pagination-controls *ngIf="miniatures && miniatures.length > 0" (pageChange)="p = $event"></pagination-controls>
-          </div>
-      </div>
+       <div class="row">
+           <div [ngClass]="miniatureClass" *ngFor="let miniature of miniatures
+            | paginate : { itemsPerPage : itemsPerPage , currentPage : p}">
+                  <miniature-show [miniature]="miniature"></miniature-show>
+           </div>
+       </div>
+       <div class="row" style="text-align:center">
+           <pagination-controls *ngIf="miniatures && miniatures.length > 0" (pageChange)="p = $event"></pagination-controls>
+       </div>
    `
 })
 
 export class MiniatureContainerComponent {
    @Input() miniatures : Miniature[];
+   @Input() miniatureClass : string = 'col-md-3';
+   @Input() itemsPerPage : number = 16;
 }
